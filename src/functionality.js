a46857bcc7065b0f67855ce6e00769c99a4e41c1
ndayiringiye david <ndayiringiyedavid120@gmail.com>
2025-02-26 11:48:12 +0200

    feat/ odd numbers checked succussfully

--- src/functionality.js
+++ src/functionality.js
@@ -10,4 +10,11 @@ console.log(FindSum(5, 5))
 
 for(let i =1 ; i <= 10; i++  ){
    console.log(i)
-}
+};
+
+// checking odd
+
+function isOdd(n){
+    return (n % 2 ===0 ? "true" : "false");
+};
+console.log(isOdd(100));
