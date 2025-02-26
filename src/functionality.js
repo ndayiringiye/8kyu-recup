c156f0550da9e0194596b1f876da80b941173f85
ndayiringiye david <ndayiringiyedavid120@gmail.com>
2025-02-26 11:34:01 +0200

    feat/ loops went well

--- src/functionality.js
+++ src/functionality.js
@@ -1,7 +1,13 @@
 const greeting = "hello world";
 console.log(greeting);
 
-function FindSum(a, b){
-   return a * b;
+function FindSum(a, b) {
+    return a * b;
 }
-console.log(FindSum(5,5))
+console.log(FindSum(5, 5))
+
+// q3
+
+for(let i =1 ; i <= 10; i++  ){
+   console.log(i)
+}
